c77b954b0719cb01c9b86a22e7ae61e61016dd04
Gilbert <gilbr@phenom>
2021-04-26 12:33:09 +0200

    git hub test

--- test/github_action_test.sh
+++ test/github_action_test.sh
@@ -29,8 +29,48 @@ apt update
 
 apt -y install debootstrapmgr
 
-debootstrapmgr pc-debootstrap -d ./test_chroot
+TEST_CHROOT_PATH=./test_chroot
 
-debootstrapmgr pc-flash -s ./test_chroot -d ./test_chroot.img
+debootstrapmgr pc-debootstrap -d $TEST_CHROOT_PATH
 
-#kvm -m 1G -nographic -drive format=raw,file=./test_chroot.img
+cat <<EOF > ${TEST_CHROOT_PATH}/root//run_test.sh
+#!/bin/sh
+
+echo "DMGR OK"
+EOF
+chmod +x ${TEST_CHROOT_PATH}/root//run_test.sh
+
+mkdir -p ${TEST_CHROOT_PATH}/etc/systemd/system/getty@ttyS0.service.d
+cat <<EOF > ${TEST_CHROOT_PATH}/etc/systemd/system/getty@ttyS0.service.d/override.conf
+[Unit]
+Description=Test debootstrapmgr
+
+[Service]
+Restart=no
+ExecStart=
+ExecStart=-/root/run_test.sh
+Type=oneshot
+RemainAfterExit=no
+StandardInput=tty
+StandardOutput=tty
+EOF
+debootstrapmgr chroot $TEST_CHROOT_PATH systemctl enable getty@ttyS0.service
+
+
+debootstrapmgr pc-flash -s $TEST_CHROOT_PATH -d ${TEST_CHROOT_PATH}.img
+
+cat <<EOF > /tmp/dmgr_expect_test
+#!/usr/bin/expect -f
+
+set timeout 180
+
+spawn qemu-system-x86_64 -nographic -m 1G $DMGR_KVM_OPTION -drive format=raw,file=${TEST_CHROOT_PATH}.img
+
+expect {
+  "DMGR OK" { exit 0 }
+  timeout { exit 1 }
+}
+EOF
+chmod +x /tmp/dmgr_expect_test
+
+/tmp/dmgr_expect_test
